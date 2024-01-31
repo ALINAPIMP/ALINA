b9a5af549499597d031ad1998ef541fd877f0944
ALINAPIMP <alina.nepikta@gmail.com>
2024-01-31 15:48:35 +0200

    APVALIAU

--- main5.py
+++ main5.py
@@ -21,24 +21,24 @@ average_salary_by_departments = df.groupby('DEPARTMENT_ID')['SALARY'].mean().rou
 employees_by_salary = df[df['SALARY']>3000]
 # print(employees_by_salary)
 
-# plt.figure(figsize = (12, 6))
-# average_salary_by_departments.plot(kind='bar')
-# plt.title('vidutine alga pagal skyriu')
-# plt.xlabel('skyrius')
-# plt.ylabel('vidutinis atlyginimas')
-# plt.xticks(rotation=0)
-# plt.show()
-
-
-df['HIRE_DATE'] = pd.to_datetime(df['HIRE_DATE'], format= '%d-%b-%y')
-df['year'] = df['HIRE_DATE'].dt.year
-employee_count_by_year = df.groupby('year').size()
-
 plt.figure(figsize = (12, 6))
-employee_count_by_year.plot(kind='line')
-plt.title('darbuotoju augimo grafikas')
-plt.xlabel('metai')
-plt.ylabel('darbuotojai')
-#plt.xticks(rotation=0)
-plt.grid(True)
+average_salary_by_departments.plot(kind='bar')
+plt.title('vidutine alga pagal skyriu')
+plt.xlabel('skyrius')
+plt.ylabel('vidutinis atlyginimas')
+plt.xticks(rotation=0)
 plt.show()
+
+
+# df['HIRE_DATE'] = pd.to_datetime(df['HIRE_DATE'], format= '%d-%b-%y')
+# df['year'] = df['HIRE_DATE'].dt.year
+# employee_count_by_year = df.groupby('year').size()
+#
+# plt.figure(figsize = (12, 6))
+# employee_count_by_year.plot(kind='line')
+# plt.title('darbuotoju augimo grafikas')
+# plt.xlabel('metai')
+# plt.ylabel('darbuotojai')
+# #plt.xticks(rotation=0)
+# plt.grid(True)
+# plt.show()
