c00d0f823f0007132a39a815d13a431e82360c9a
ALINAPIMP <alina.nepikta@gmail.com>
2024-02-01 12:44:52 +0200

    vizualizacija pagal vaistinės asortimentą

--- main5.py
+++ main5.py
@@ -18,17 +18,24 @@ filtered_df = df['DEPARTMENT_ID'].value_counts
 average_salary_by_departments = df.groupby('DEPARTMENT_ID')['SALARY'].mean().round(2)
 # print(average_salary_by_departments)
 
+
 employees_by_salary = df[df['SALARY']>3000]
 # print(employees_by_salary)
 
-plt.figure(figsize = (12, 6))
-average_salary_by_departments.plot(kind='bar')
-plt.title('vidutine alga pagal skyriu')
-plt.xlabel('skyrius')
-plt.ylabel('vidutinis atlyginimas')
-plt.xticks(rotation=0)
+# plt.figure(figsize = (12, 6))
+# average_salary_by_departments.plot(kind='bar')
+# plt.title('vidutine alga pagal skyriu')
+# plt.xlabel('skyrius')
+# plt.ylabel('vidutinis atlyginimas')
+# plt.xticks(rotation=0)
+# plt.show()
+plt.figure(figsize=(12,6))
+plt.bar(sorted_products['Title'], sorted_products['Price'], sorted_products['Bedroom'], color='teal')
+plt.xlabel('Title')
+plt.ylabel('Price(€)')
+plt.title('Top 10 most expensive products')
+plt.xticks(rotation=90)
 plt.show()
-
 
 # df['HIRE_DATE'] = pd.to_datetime(df['HIRE_DATE'], format= '%d-%b-%y')
 # df['year'] = df['HIRE_DATE'].dt.year
